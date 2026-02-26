4c688ef2fc36e2e658e736c62412af696fa2c96b
ZhaoyangDong-code <zhaoyan7@ualberta.ca>
2026-02-25 18:45:11 -0700

    301 Lab7 commit

--- app/src/main/java/com/example/androiduitesting/MainActivity.java
+++ app/src/main/java/com/example/androiduitesting/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.androiduitesting;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -30,11 +32,7 @@ public class MainActivity extends AppCompatActivity {
 
         cityList = findViewById(R.id.city_list);
 
-        //String []cities ={"Edmonton", "Vancouver", "Moscow", "Sydney", "Berlin", "Vienna", "Tokyo", "Beijing", "Osaka", "New Delhi"};
-
         dataList = new ArrayList<>();
-
-        //dataList.addAll(Arrays.asList(cities));
 
         cityAdapter = new ArrayAdapter<>(this, R.layout.content, dataList);
 
@@ -62,6 +60,16 @@ public class MainActivity extends AppCompatActivity {
         deleteButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 cityAdapter.clear();
+            }
+        });
+
+        cityList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String cityName = dataList.get(position);
+                Intent intent = new Intent(MainActivity.this, ShowActivity.class);
+                intent.putExtra("CITY_NAME", cityName);
+                startActivity(intent);
             }
         });
     }
